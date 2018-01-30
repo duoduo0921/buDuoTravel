b26e3536155509f6fd7eb9234c8ca16b03dd1209
duoduo0921 <wang.hand@husky.neu.edu>
2018-01-29 21:45:40 -0500

    Update flight.ex

--- lib/buDuoTravel/travel/flight.ex
+++ lib/buDuoTravel/travel/flight.ex
@@ -13,7 +13,7 @@ defmodule BuDuoTravel.Travel.Flight do
   end
 
    def flight_list(ori, des, date) do
-    url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=u9SJXmfraFHyehPZNd1BwukF43eXJGc3&origin=#{ori}&destination=#{des}&departure_date=#{date}"
+    url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=prBuA68WLoPMGqJ91KtRArlYap50quAB&origin=#{ori}&destination=#{des}&departure_date=#{date}"
     resp = HTTPoison.get!(url, [connect_timeout: 50_000, timeout: 50_000, recv_timeout: 50_000])
     data = Poison.decode!(resp.body)
     data["results"]
